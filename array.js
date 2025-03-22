bf3935674567c1b55de31fe378898f55aecaf409
tonmoy-free <tonmoykhan.free@gmail.com>
2025-03-22 10:07:41 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,21 @@
+const friends = ["hero", "JKR", "SRK", "Rahim","jalal"];
+
+
+friends.push("bd"); // array ar ses a "bd" add hobe.
+
+friends.pop(); // array ar ses ar element bad dibe.
+
+friends.unshift("Korim"); // array ar surute  add hobe.
+
+friends.shift(); // array ar first element bab dibe.
+
+friends.slice(1,2);
+
+console.log(friends.slice(1,3)); // 3 idex age porjonto dibe.
+
+console.log(friends.splice(1,3)) // 3 index porjonto dibe.
+
+// console.log(friends);
+
+
+
