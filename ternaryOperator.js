9e29583da565d8415fe8da7d04dcdb06818d2183
tonmoy-free <tonmoykhan.free@gmail.com>
2025-03-22 21:49:50 +0600

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,5 @@
+// condition ? exprIfTrue : exprIfFalse
+
+const age = 26;
+const beverage = age >= 21 ? "Beer" : "Juice"; // 21 ar somane ba choto hole "Beer" print korbe. 21 boro hole "juce" print korbe.
+console.log(beverage); // "Beer"
