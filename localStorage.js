45a1df1c533adc663dfd0e35f18cf9d72e152afa
tonmoy-free <tonmoykhan.free@gmail.com>
2025-03-22 22:17:52 +0600

    localStorage

---
+++ localStorage.js
@@ -0,0 +1,8 @@
+localStorage.setItem("myCat", "Tom"); // set ba save item on browser. Key value
+
+const cat = localStorage.getItem("myCat"); // set kora item ke pawar jonno get function babohar hoi.
+
+
+localStorage.removeItem("myCat"); // set kora ja kono item ke delete ba remove korte babohar hoi.
+
+localStorage.clear(); //set kora sob item ke delete ba remove korte babohar hoi.
