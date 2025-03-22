0bd2f10a672754a323504346ed29e87177c87e9a
tonmoy-free <tonmoykhan.free@gmail.com>
2025-03-22 21:42:32 +0600

    truthy values

---
+++ truthyValues.js
@@ -0,0 +1,17 @@
+// Examples of truthy values in JavaScript (which will be coerced to true in boolean contexts, and thus execute the if block):
+
+// js
+// Copy to Clipboard
+// if (true)
+// if ({})
+// if ([])
+// if (42)
+// if ("0")
+// if ("false")
+// if (new Date())
+// if (-42)
+// if (12n)
+// if (3.14)
+// if (-3.14)
+// if (Infinity)
+// if (-Infinity)
